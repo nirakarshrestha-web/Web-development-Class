a8aa16b3af8f70891e90a59614553ffc2d08350a
nirakarshrestha-web <nirakar.shrestha@westcliff.edu>
2026-03-13 15:24:56 +0545

    jjsclass

---
+++ JS-Class 1/Js/script.js
@@ -0,0 +1,14 @@
+
+var x = 25;
+var y = 4;
+var mulitple = x * y;
+var remainder = x % y;
+let a = 7;
+if (true){
+    console.log(a);
+    let a  = 7;
+    console.log(a);
+}
+console.log('The muliple is',mulitple);
+console.log('the remainder is', remainder);
+console.log(a);
